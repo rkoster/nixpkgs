240bf4e2f1db4195a293614cb3162b2e235fc54d
rkoster <rkoster@starkandwayne.com>
2020-11-26 15:36:16 +0100

    Switch to lsp-mode and use-package

--- program/emacs/default.nix
+++ program/emacs/default.nix
@@ -4,16 +4,27 @@
   enable = true;
   package = pkgs.emacs-nox;
   extraPackages = epkgs: [
+    epkgs.use-package
+    # util
     epkgs.magit
+    epkgs.wgrep
+    epkgs.company
+    epkgs.flycheck
+    epkgs.which-key
+    epkgs.projectile
+    epkgs.whitespace-cleanup-mode
+    
+    # lang
+    epkgs.lsp-mode
+    epkgs.lsp-ui    
     epkgs.go-mode
-    epkgs.go-guru
     epkgs.nix-mode
-    epkgs.nixpkgs-fmt
     epkgs.yaml-mode
-    epkgs.flycheck-yamllint
+
+    # visual
     epkgs.gruvbox-theme
     epkgs.powerline
     epkgs.powerline-evil
-  ];
+   ];
 }
 
--- program/emacs/init.el
+++ program/emacs/init.el
@@ -1,9 +1,66 @@
 ;; Keybindings
-(global-set-key (kbd "C-x g") 'magit-status)
+;; (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; Theme
 (load-theme 'gruvbox-dark-medium t)
 
-(require 'powerline)
-(powerline-default-theme)
-(require 'powerline-evil)
+(use-package powerline
+  :config
+  (powerline-default-theme))
+
+(use-package wgrep)
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+;;; linum
+(use-package linum
+  :config
+  (add-hook 'prog-mode-hook 'linum-mode)
+  (setq linum-format "%4d "))
+
+;; (defun my-change-window-divider ()
+;;   (let ((display-table (or buffer-display-table standard-display-table)))
+;;     (set-display-table-slot display-table 5 ?│)
+;;     (set-window-display-table (selected-window) display-table)))
+
+;; (add-hook 'window-configuration-change-hook 'my-change-window-divider)
+
+(use-package whitespace-cleanup-mode
+  :hook (go-mode whitespace-cleanup-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;; Optional - provides fancier overlays.
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-eldoc-enable-hover nil)
+  :commands lsp-ui-mode)
+
+;; Company mode is a standard completion package that works well with lsp-mode.
+(use-package company
+  :ensure t
+  :config
+  ;; Optionally enable completion-as-you-type behavior.
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+(use-package projectile
+  :config
+  (projectile-mode +1))
+
+(use-package which-key
+  :config  
+  (which-key-mode))
