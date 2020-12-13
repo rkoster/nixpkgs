868eae0742c861f4a429f3e048b7a7ca88c322b8
rkoster <rkoster@starkandwayne.com>
2020-12-13 22:46:14 +0100

    [Alacritty] Meta keybindings + gruvbox colors

--- programs/alacritty/default.nix
+++ programs/alacritty/default.nix
@@ -1,14 +1,12 @@
 {
   enable = true;
   settings = {
-    live_config_reload = true;
-
     env = {
       "TERM" = "xterm-256color";
     };
 
     font = {
-      size = 12.0;
+      size = 13.0;
       use_thin_strokes = true;
 
       normal.family = "Hack";
@@ -23,39 +21,119 @@
       save_to_clipboard = true;
     };
 
-    # Nord
+    # Colors (Gruvbox dark)
     colors = {
+      # Default colors
       primary = {
-        background = "0x2E3440";
-        foreground = "0xD8DEE9";
+        # hard contrast: background = '#1d2021'
+        background = "#282828";
+        # soft contrast: background = "#32302f"
+        foreground = "#ebdbb2";
       };
-
-      cursor = {
-        text = "0x2E3440";
-        cursor = "0xD8DEE9";
-      };
-
+      # Normal colors
       normal = {
-        black = "0x3B4252";
-        red = "0xBF616A";
-        green = "0xA3BE8C";
-        yellow = "0xEBCB8B";
-        blue = "0x81A1C1";
-        magenta = "0xB48EAD";
-        cyan = "0x88C0D0";
-        white = "0xE5E9F0";
+        black =   "#282828";
+        red =     "#cc241d";
+        green =   "#98971a";
+        yellow =  "#d79921";
+        blue =    "#458588";
+        magenta = "#b16286";
+        cyan =    "#689d6a";
+        white =   "#a89984";
       };
 
+      # Bright colors
       bright = {
-        black = "0x4C566A";
-        red = "0xBF616A";
-        green = "0xA3BE8C";
-        yellow = "0xEBCB8B";
-        blue = "0x81A1C1";
-        magenta = "0xB48EAD";
-        cyan = "0x8FBCBB";
-        white = "0xECEFF4";
+        black =   "#928374";
+        red =     "#fb4934";
+        green =   "#b8bb26";
+        yellow =  "#fabd2f";
+        blue =    "#83a598";
+        magenta = "#d3869b";
+        cyan =    "#8ec07c";
+        white =   "#ebdbb2";
       };
     };
+
+    key_bindings = [
+      { key = "A";         mods = "Alt|Shift"; chars = "\\x1bA"; }
+      { key = "B";         mods = "Alt|Shift"; chars = "\\x1bB"; }
+      { key = "C";         mods = "Alt|Shift"; chars = "\\x1bC"; }
+      { key = "D";         mods = "Alt|Shift"; chars = "\\x1bD"; }
+      { key = "E";         mods = "Alt|Shift"; chars = "\\x1bE"; }
+      { key = "F";         mods = "Alt|Shift"; chars = "\\x1bF"; }
+      { key = "G";         mods = "Alt|Shift"; chars = "\\x1bG"; }
+      { key = "H";         mods = "Alt|Shift"; chars = "\\x1bH"; }
+      { key = "I";         mods = "Alt|Shift"; chars = "\\x1bI"; }
+      { key = "J";         mods = "Alt|Shift"; chars = "\\x1bJ"; }
+      { key = "K";         mods = "Alt|Shift"; chars = "\\x1bK"; }
+      { key = "L";         mods = "Alt|Shift"; chars = "\\x1bL"; }
+      { key = "M";         mods = "Alt|Shift"; chars = "\\x1bM"; }
+      { key = "N";         mods = "Alt|Shift"; chars = "\\x1bN"; }
+      { key = "O";         mods = "Alt|Shift"; chars = "\\x1bO"; }
+      { key = "P";         mods = "Alt|Shift"; chars = "\\x1bP"; }
+      { key = "Q";         mods = "Alt|Shift"; chars = "\\x1bQ"; }
+      { key = "R";         mods = "Alt|Shift"; chars = "\\x1bR"; }
+      { key = "S";         mods = "Alt|Shift"; chars = "\\x1bS"; }
+      { key = "T";         mods = "Alt|Shift"; chars = "\\x1bT"; }
+      { key = "U";         mods = "Alt|Shift"; chars = "\\x1bU"; }
+      { key = "V";         mods = "Alt|Shift"; chars = "\\x1bV"; }
+      { key = "W";         mods = "Alt|Shift"; chars = "\\x1bW"; }
+      { key = "X";         mods = "Alt|Shift"; chars = "\\x1bX"; }
+      { key = "Y";         mods = "Alt|Shift"; chars = "\\x1bY"; }
+      { key = "Z";         mods = "Alt|Shift"; chars = "\\x1bZ"; }
+
+      { key = "A";         mods = "Alt"; chars = "\\x1ba"; }
+      { key = "B";         mods = "Alt"; chars = "\\x1bb"; }
+      { key = "C";         mods = "Alt"; chars = "\\x1bc"; }
+      { key = "D";         mods = "Alt"; chars = "\\x1bd"; }
+      { key = "E";         mods = "Alt"; chars = "\\x1be"; }
+      { key = "F";         mods = "Alt"; chars = "\\x1bf"; }
+      { key = "G";         mods = "Alt"; chars = "\\x1bg"; }
+      { key = "H";         mods = "Alt"; chars = "\\x1bh"; }
+      { key = "I";         mods = "Alt"; chars = "\\x1bi"; }
+      { key = "J";         mods = "Alt"; chars = "\\x1bj"; }
+      { key = "K";         mods = "Alt"; chars = "\\x1bk"; }
+      { key = "L";         mods = "Alt"; chars = "\\x1bl"; }
+      { key = "M";         mods = "Alt"; chars = "\\x1bm"; }
+      { key = "N";         mods = "Alt"; chars = "\\x1bn"; }
+      { key = "O";         mods = "Alt"; chars = "\\x1bo"; }
+      { key = "P";         mods = "Alt"; chars = "\\x1bp"; }
+      { key = "Q";         mods = "Alt"; chars = "\\x1bq"; }
+      { key = "R";         mods = "Alt"; chars = "\\x1br"; }
+      { key = "S";         mods = "Alt"; chars = "\\x1bs"; }
+      { key = "T";         mods = "Alt"; chars = "\\x1bt"; }
+      { key = "U";         mods = "Alt"; chars = "\\x1bu"; }
+      { key = "V";         mods = "Alt"; chars = "\\x1bv"; }
+      { key = "W";         mods = "Alt"; chars = "\\x1bw"; }
+      { key = "X";         mods = "Alt"; chars = "\\x1bx"; }
+      { key = "Y";         mods = "Alt"; chars = "\\x1by"; }
+      { key = "Z";         mods = "Alt"; chars = "\\x1bz"; }
+
+      #   { key = "Key1";      mods = "Alt";       chars = "\\x1b1"; }
+      #   { key = "Key2";      mods = "Alt";       chars = "\\x1b2"; }
+      #   { key = "Key3";      mods = "Alt";       chars = "\\x1b3"; }
+      #   { key = "Key4";      mods = "Alt";       chars = "\\x1b4"; }
+      #   { key = "Key5";      mods = "Alt";       chars = "\\x1b5"; }
+      #   { key = "Key6";      mods = "Alt";       chars = "\\x1b6"; }
+      #   { key = "Key7";      mods = "Alt";       chars = "\\x1b7"; }
+      #   { key = "Key8";      mods = "Alt";       chars = "\\x1b8"; }
+      #   { key = "Key9";      mods = "Alt";       chars = "\\x1b9"; }
+      #   { key = "Key0";      mods = "Alt";       chars = "\\x1b0"; }
+      #   { key = "Space";     mods = "Control";   chars = "\\x00";  } # Ctrl + Space
+      #   { key = "Grave";     mods = "Alt";       chars = "\\x1b`"; } # Alt + `
+      #   { key = "Grave";     mods = "Alt|Shift"; chars = "\\x1b~"; } # Alt + ~
+      #   { key = "Period";    mods = "Alt";       chars = "\\x1b."; } # Alt + .
+      #   { key = "Key8";      mods = "Alt|Shift"; chars = "\\x1b*"; } # Alt + *
+      #   { key = "Key3";      mods = "Alt|Shift"; chars = "\\x1b#"; } # Alt + #
+      { key = "Period";    mods = "Alt|Shift"; chars = "\\x1b>"; } # Alt + >
+      { key = "Comma";     mods = "Alt|Shift"; chars = "\\x1b<"; } # Alt + <
+      { key = "Slash";     mods = "Alt"; chars = "\\x1b/"; } # Alt + <
+      #   { key = "Minus";     mods = "Alt|Shift"; chars = "\\x1b_"; } # Alt + _
+      #   { key = "Key5";      mods = "Alt|Shift"; chars = "\\x1b%"; } # Alt + %
+      #   { key = "Key6";      mods = "Alt|Shift"; chars = "\\x1b^"; } # Alt + ^
+      #   # { key = "Backslash"; mods = "Alt";       chars = "\x1b\\";} # Alt + \
+      #   # { key = "Backslash"; mods = "Alt|Shift"; chars = "\x1b|"; } # Alt + |~
+    ];
   };
 }
