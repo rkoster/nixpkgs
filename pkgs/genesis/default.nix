166777599db40cae0aa23bce3c4a8097e5b804bf
rkoster <rkoster@starkandwayne.com>
2021-02-11 21:38:12 +0100

    Update genesis

--- pkgs/genesis/default.nix
+++ pkgs/genesis/default.nix
@@ -1,7 +1,7 @@
 { which, perl, git, jq, curl, spruce, safe, vault, bosh, fetchFromGitHub, stdenv, lib, writeText }:
 
 let
-  version = "2.7.21";
+  version = "2.7.30";
 in stdenv.mkDerivation {
   name = "genesis";
 
@@ -9,7 +9,7 @@ in stdenv.mkDerivation {
     owner = "genesis-community";
     repo = "genesis";
     rev =  "v${version}";
-    sha256 = "0sbqaa1gixanzpf9kx24h7k391gg0275j1bwjhpm5ig71s73gb3y";
+    sha256 = "1s8247w4m3qp9wpq7gr1bl2bbp939b908fv1vgkrci9fa8f5qyj8";
   };
 
   buildInputs = [ which perl git jq curl spruce safe vault bosh ];
