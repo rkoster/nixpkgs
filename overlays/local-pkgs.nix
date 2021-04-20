85f66986b28ddb05a26cb93b6a7d8dea97106f08
rkoster <hi@rkoster.dev>
2021-04-20 13:14:55 +0200

    Use upstream carvel packages

--- overlays/local-pkgs.nix
+++ overlays/local-pkgs.nix
@@ -5,8 +5,6 @@ self: super:
   leftovers = super.callPackage ../pkgs/leftovers { };
   bosh = super.callPackage ../pkgs/bosh { };
   boshBootloader = super.callPackage ../pkgs/bosh-bootloader { };
-  ytt = super.callPackage ../pkgs/ytt { };
-  vendir = super.callPackage ../pkgs/vendir { };
   cf = super.callPackage ../pkgs/cf { };
   spruce = super.callPackage ../pkgs/spruce { };
   safe = super.callPackage ../pkgs/safe { };
