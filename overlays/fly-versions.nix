cce278f86c9b71c2d1002413228c41fd5f01b421
rkoster <hi@rkoster.dev>
2021-05-14 15:03:47 +0200

    Bump fly64 to 6.4.1

--- overlays/fly-versions.nix
+++ overlays/fly-versions.nix
@@ -42,6 +42,6 @@ in {
   # nix-build '<nixpkgs>' -A fly60 # will get you the real sha
   fly72 = fly "7.2.0" "1l3a9qhrdqk462fv2r7lcq5s725v5bv824wivc1sn9m03pkcvb5q" "1ljnn0swv9zv2kxa7g341iy5pbm3zjmq88c4k0zhsm4gag5dgyyq";
   fly67 = fly "6.7.5" "15nnnsq75s7139nna950p15xr73ssi37p7kxczg5p28s3gz23gx4" "08i1hpg1p6yrwh2vi29gm8z9kcw2z5jqvb08cmmy8mm2b5h19hi1";
-  fly64 = fly "6.4.0" "08lw345kzkic5b2dqj3d0d9x1mas9rpi4rdmbhww9r60swj169i7" "0a78cjfj909ic8wci8id2h5f6r34h90myk6z7m918n08vxv60jvw";
+  fly64 = fly "6.4.1" "16si1qm835yyvk2f0kwn9wnk8vpy5q4whgws1s2p6jr7dswg43h8" "0nv9q3j9cja8c6d7ac8fzb8zf82zz1z77f8cxvn3vxjki7fhlavm";
   fly60 = fly "6.0.0" "0chavwymyh5kv4fkvdjvf3p5jjx4yn9aavq66333xnsl5pn7q9dq" "127mc1wzqhn0l4ni6qxcx06qfdb1cgahzypjrs4vgr6i4sipjxck";
 }
