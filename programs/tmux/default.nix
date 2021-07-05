49b6bb5327b08e5a2f5bdfb73437089d78c68e93
rkoster <hi@rkoster.dev>
2021-07-05 12:57:32 +0200

    Added tail all bosh logs shortcut + remove unused config

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -6,19 +6,12 @@
   clock24 = true;
   historyLimit = 5000;
   terminal = "screen-256color";
-  plugins = with pkgs; [
-    tmuxPlugins.gruvbox
-    tmuxPlugins.battery
-    tmuxPlugins.net-speed
-    tmuxPlugins.cpu
-  ];
+  plugins = [ pkgs.tmuxPlugins.gruvbox ];
   extraConfig = ''
-    # set -g status-right "#{cpu_bg_color} CPU: #{cpu_icon} #{cpu_percentage} | #(battery_percentage) | %a %h-%d %H:%M "
     set -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
-    set -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d  %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237]  #(ram_percentage) ﬙     "
+    set -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] %H:%M "
 
-    bind -N "bosh create and upload release" -T root M-R send-keys "bosh -n create-release --force && bosh -n upload-release\n"
-
-#   set-hook -g session-created 'split -h ; split -v top'
+    bind -N "bosh create and up2load release" -T root M-R send-keys "bosh -n create-release --force && bosh -n upload-release\n"
+    bind -N "tail all bosh logs" -T root M-L send-keys "find /var/vcap/{sys/log,bosh/log,monit} -name *.log -or -name current | xargs tail -f"
   '';
 }
