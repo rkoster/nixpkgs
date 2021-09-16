5bc9f2a06dea18ff486b0d822f5fd6384c70342a
rkoster <hi@rkoster.dev>
2021-09-16 14:27:00 +0200

    Fix tail all bosh logs

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -12,6 +12,6 @@
     set -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] %H:%M "
 
     bind -N "bosh create and up2load release" -T root M-R send-keys "bosh -n create-release --force && bosh -n upload-release\n"
-    bind -N "tail all bosh logs" -T root M-L send-keys "find /var/vcap/{sys/log,bosh/log,monit} -name *.log -or -name current | xargs tail -f"
+    bind -N "tail all bosh logs" -T root M-L send-keys "find /var/vcap/{sys/log,bosh/log,monit} -name *.log -or -name current | xargs tail -f\n"
   '';
 }
