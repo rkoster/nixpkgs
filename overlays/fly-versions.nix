2bf3a17be6ec9b839398d620289559214a2ce149
rkoster <hi@rkoster.dev>
2021-09-30 14:49:39 +0200

    Added fly 7.5.0

--- overlays/fly-versions.nix
+++ overlays/fly-versions.nix
@@ -40,6 +40,7 @@ let
 in {
   # fly60 = fly "6.0.0" "0chavwymyh5kv4fkvdjvf3p5jjx4yn9aavq66333xnsl5pn7q9dq" super.lib.fakeSha256;
   # nix-build '<nixpkgs>' -A fly60 # will get you the real sha
+  fly75 = fly "7.5.0" "1085gxjrc5fh6a1j2cjcv3h4na4cabcliw6isgf0aimqz4ic1v77" "0dhcs5ma968bii2np51zbib2kvc8g8cpjkwzvnzgpmz7pi4z3b37";
   fly74 = fly "7.4.0" "0hy5sndqnbci42wc336hand9xkqkf6q5xdchxmjknnshkd6hzwaj" "02dknyv3nxy55dspdcv321x4db8hfxqmz0jivkijyd8a18d8g3r1";
   fly73 = fly "7.3.2" "0h6znpj5fmgjqpqcbvsv7kw6fi310lam7iw8pbg74a3k86mfygr0" "0g1rjs7ss0q5j9hbz5kykrkvl1sg6nxl82jay8mln7y88d3fnjnz";
   fly72 = fly "7.2.0" "1l3a9qhrdqk462fv2r7lcq5s725v5bv824wivc1sn9m03pkcvb5q" "1ljnn0swv9zv2kxa7g341iy5pbm3zjmq88c4k0zhsm4gag5dgyyq";
