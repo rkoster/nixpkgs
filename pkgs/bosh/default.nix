7593d275bded6bc89dc4c45e23fe4c7be98134cb
rkoster <hi@rkoster.dev>
2024-01-18 16:14:56 +0100

    Update bosh-cli and remove --no-mac-metadata tar flag on nix

--- pkgs/bosh/default.nix
+++ pkgs/bosh/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "bosh";
-  version = "7.5.0";
+  version = "7.5.2";
 
   src = fetchFromGitHub {
     owner = "cloudfoundry";
     repo = "bosh-cli";
     rev = "v${version}";
-    sha256 = "sha256-LWfS722V0ubh4QaUNuGwrvFsVAzA9Hd5xLlDfTG0PBs=";
+    sha256 = "sha256-gT0Oivo5QE+pr5PpD/7JAj8oYF9UmSi5F6Ps8RtACzc=";
   };
 
   vendorHash = null;
@@ -16,7 +16,8 @@ buildGoModule rec {
   doCheck = false;
 
   preBuild = ''
-    sed 's/\[DEV BUILD\]/'"${version}-nix"'/' cmd/version.go > cmd/version.tmp && mv cmd/version{.tmp,.go}
+    sed -i 's/\[DEV BUILD\]/'"${version}-nix"'/' cmd/version.go
+    sed -i 's/darwin/disable/' vendor/github.com/cloudfoundry/bosh-utils/fileutil/tarball_compressor.go
   '';
 
   subPackages = [ "main.go" ];
@@ -24,7 +25,14 @@ buildGoModule rec {
   postBuild = ''
      cd "$GOPATH/bin"
      mv main bosh
-  ''; 
+  '';
+
+  # postInstall = ''
+  #   installShellCompletion --cmd aws \
+  #     --zsh $out/bin/aws_zsh_completer.sh
+  # '' + lib.optionalString (!stdenv.hostPlatform.isWindows) ''
+  #   rm $out/bin/aws.cmd
+  # '';
 
   meta = with lib; {
     description = "BOSH CLI v2+";
