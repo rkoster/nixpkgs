06b58d2a268f4e6019abb6395c2450362b37de80
rkoster <hi@rkoster.dev>
2024-02-22 18:38:26 +0100

    Update bosh cli

--- pkgs/bosh/default.nix
+++ pkgs/bosh/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "bosh";
-  version = "7.5.2";
+  version = "7.5.3";
 
   src = fetchFromGitHub {
     owner = "cloudfoundry";
     repo = "bosh-cli";
     rev = "v${version}";
-    sha256 = "sha256-gT0Oivo5QE+pr5PpD/7JAj8oYF9UmSi5F6Ps8RtACzc=";
+    sha256 = "sha256-G9622QudxCUKJpHoezfpSjLFsX9H4m65lacoMATWyAA=";
   };
 
   vendorHash = null;
