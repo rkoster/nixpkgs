24067fad1c6c9721b247248300b96138cc14e53e
rkoster <hi@rkoster.dev>
2024-11-18 18:27:20 +0100

    Fix kitty for Sequoia

--- programs/kitty/default.nix
+++ programs/kitty/default.nix
@@ -1,7 +1,17 @@
 { pkgs }:
 
-{
+# apply: https://github.com/NixOS/nixpkgs/pull/352795
+let
+     unstablePkgs = import (builtins.fetchGit {
+         # Descriptive name to make the store path easier to identify
+         name = "my-old-revision";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "62cfb3e8d8b15ed71217b68c526ea3ecefd6acc2";
+     }) {};
+in {
   enable = true;
+  package = unstablePkgs.kitty;
   environment = {
     "TERM" = "xterm-256color";
   };
