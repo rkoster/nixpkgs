e9a8f8f5aeea88349f370200f4c7f50fd775ef57
rkoster <hi@rkoster.dev>
2025-01-17 14:30:15 +0100

    Bump smith cli

--- pkgs/smith/default.nix
+++ pkgs/smith/default.nix
@@ -2,20 +2,21 @@
 
 buildGoModule rec {
   pname = "smith";
-  version = "2.6.0";
+  version = "2.9.0";
 
   # use fetchGit since it supports using ssh private keys for auth
   src = builtins.fetchGit {
-    url = "git@github.com:pivotal/smith.git";
-    ref = "master";
-    rev = "0c178e7c3a998b774814621e97deb257674e356f"; # v2.6.0
+    url = "git@github.gwd.broadcom.net:TNZ/smith.git";
+    ref = "main";
+    rev = "7f4bca2dc48b40bd54e1a780b152579b3fc57768"; # v2.9.0
   };
 
-  vendorHash = null;
+  vendorHash = "sha256-cCi6S0VSRJKl2jchL85nSTQhUzXuJOX+KuLnBrpD5i8=";
 
   doCheck = false;
 
   preBuild = ''
+    sed 's/go 1.23.*/go 1.22.0/g' go.mod > go.mod.tmp && mv go.mod{.tmp,}
     sed 's/development build/'"${version}-nix"'/' main.go > main.tmp && mv main{.tmp,.go}
   '';
 
