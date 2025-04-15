5bec89e70eda056a8ea44209e509a35ec4d0780e
rkoster <hi@rkoster.dev>
2025-04-15 10:49:48 +0200

    Switch back to upstream & use lvn endpoint by default

--- pkgs/shepherd/default.nix
+++ pkgs/shepherd/default.nix
@@ -2,25 +2,22 @@
 
 buildGo123Module rec {
   pname = "shepherd";
-  version = "0.7.9";
+  version = "0.7.8";
 
   src = builtins.fetchGit {
-    #url = "git@github.gwd.broadcom.net:TNZ/shepherd2.git";
-    url = "/Users/rubenk/workspace/shepherd2";
-    #ref = "main";
-    ref = "bump-cli-deps";
-    # rev = "b2e46d058c2b3e164096b53ec6b2b3678f7eee8d";
-    rev = "6a566c6741b420574adcefb1d3cefdaf1d460abd";
-  }; 
+    url = "git@github.gwd.broadcom.net:TNZ/shepherd2.git";
+    ref = "main";
+    rev = "86366133cf28125e0c5135cb392551055945e929";
+  };
 
-  # vendorHash = "sha256-m5E+05OZW1THyjjHS6SnLJU4+2jeO+SMWLPkotVJ7SM=";
   vendorHash = "sha256-wwucAdto5XpH18Tz1CObsPJqe5DZxxSCd870F/BvMbc=";
 
   doCheck = false;
 
   ldflags = [
     "-X gitlab.eng.vmware.com/shepherd/shepherd2/client/cli/cmd.Version=${version}"
-    "-X gitlab.eng.vmware.com/shepherd/shepherd2/client/cli/cmd.DefaultLocation=https://v2.shepherd.run"
+    # "-X gitlab.eng.vmware.com/shepherd/shepherd2/client/cli/cmd.DefaultLocation=https://v2.shepherd.run"
+    "-X gitlab.eng.vmware.com/shepherd/shepherd2/client/cli/cmd.DefaultLocation=https://v2-shepherd.lvn.broadcom.net"
   ];
 
   subPackages = [ "main.go" ];
